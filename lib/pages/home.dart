d38320923eb62993cba12bf04dc3c10386668022
Joseph Tastet <Josephwtastet@gmail.com>
2021-03-16 20:43:12 -0400

    added spin the wheel button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -31,24 +31,55 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: <Widget>[
+          Container(height: 25),
           Text('Thank you for using my Capstone app!',
+          textAlign: TextAlign.center,
           style: TextStyle(
             fontSize: 25,
           )),
+          Container(
+            height: 50,
+          ),
 
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.fitWidth, // otherwise the logo will be tiny
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Container(
+              width: 300,
+              height: 75,
               child: ElevatedButton(
                 onPressed: () {
                   print('lets get started, JT 316');
                   Navigator.pushNamed(context, '/beverageTypes', arguments: data);
                 },
-                child: Text('"I am Thirsty!"'),
+                child: Text('Find a Drink',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
                 style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent)),
               ),
             ),
-          ),
+          ]),
+          Container(height: 50),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Container(
+                width: 300,
+                height: 75,
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('lets get started, JT 316');
+                    Navigator.pushNamed(context, '/beverageTypes', arguments: data);
+                  },
+                  child: Text('Spin the Wheel!',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent)),
+                ),
+              ),
+              ]),
         ],
       )
     );
